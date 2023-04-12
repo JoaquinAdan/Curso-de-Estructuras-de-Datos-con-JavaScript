9dc18d8f628d19f90e2e314b689aa5324a7c9e95
JoaquinAdan <rodriguezjdev@gmail.com>
2023-04-12 15:13:47 -0300

    nose

--- hashTable/hashtable.js
+++ hashTable/hashtable.js
@@ -17,10 +17,58 @@ class HashTable {
     this.data[adress].push([key, value]);
     return this.data;
   }
+  get(key) {
+    const adress = this.hashMethod(key);
+    const currentBucket = this.data[adress];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1]; // con el primer [i] entra en la lista y con el segundo [1] elige key o value, key = [0], value = [1]
+        }
+      }
+    }
+    return undefined;
+  }
+  getAllKeys() {
+    const keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          console.log(this.data[i])
+          keys.push(this.data[i][j][0]);
+        }
+      }
+    }
+    return keys;
+  }
+  delete(key) {
+    //* obtengo el hash del address
+    const address = this.hashMethod(key);
+    //* Obtengo el bucket donde debo buscar el espacio a eliminar
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      //* recorriendo el espacio
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          //* guardo el espacio para retornarlo luego
+          let deletedSpace = currentBucket[i];
+          //* elimino el espacio
+          delete currentBucket[i];
+          //* elimino el espacio vacio para que los demas espacios recorran
+          currentBucket.splice(i, 1);
+          return deletedSpace;
+        }
+      }
+    }
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('Diego', 1990);
 myHashTable.set('Joaquin', 1998);
 myHashTable.set('Victoria', 1998);
-console.log(myHashTable);
+
+console.log(myHashTable.getAllKeys());
+// console.log(myHashTable.get('Diego'));
+// console.log(myHashTable);
---
+++ linkedList/singly.js
@@ -0,0 +1,54 @@
+// 1 --> 2 --> 3 --> 4 --> 5 --> null
+
+let singlyLinkedList = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class MySingleLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head
+    this.length = 1
+  }
+  checkNode(node){
+    if(!node.next){
+      return node;
+    }
+    return this.checkNode(node.next);
+  }
+  append(val){
+    const newNode = new Node(val);
+    const lastNode = this.checkNode(this.head);
+    lastNode.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this.head;
+  }
+}
+
+let myLinkedList =  new MySingleLinkedList(1);
+myLinkedList.append(2)
+myLinkedList.append(3)
+console.log(myLinkedList);
