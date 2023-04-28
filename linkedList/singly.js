097303db9ee4d753ae78ce7ab3cc63fab53fab23
JoaquinAdan <rodriguezjdev@gmail.com>
2023-04-28 07:52:04 -0300

    idk

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -1,20 +1,20 @@
 // 1 --> 2 --> 3 --> 4 --> 5 --> null
 
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null
-        }
-      }
-    }
-  }
-}
+// let singlyLinkedList = {
+//   head: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: null
+//         }
+//       }
+//     }
+//   }
+// }
 
 class Node {
   constructor(value) {
